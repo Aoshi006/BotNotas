906b99a9f386f023038eca3637e40c0f2bafe16f
Aoshi006 <111211697+Aoshi006@users.noreply.github.com>
2026-09-19 19:49:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,35 @@
-const CACHE="prisma-web-v2026-09-19-1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./config.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{
-  if(new URL(e.request.url).origin!==location.origin)return;
-  e.respondWith(fetch(e.request).then(r=>{
-    const c=r.clone();
-    caches.open(CACHE).then(cache=>cache.put(e.request,c));
-    return r;
-  }).catch(()=>caches.match(e.request)));
+const CACHE="prisma-web-v2026-09-19-1944";
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./styles.css?v=20260919-1944",
+  "./app.js?v=20260919-1944",
+  "./config.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch",event=>{
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin) return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html")))
+  );
 });
